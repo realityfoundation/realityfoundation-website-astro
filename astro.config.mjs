edcafdee0a5637fadfb5c38c76dbffa786a54060
Emilio Dalla Torre <info@emiliodallatorre.it>
2026-05-03 00:31:30 +0200

    Empty Monolume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
-// @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+import { SITE } from "./src/consts";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://realityfoundation.eu',
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: SITE.URL,
+  vite: {
+    root: process.cwd(),
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      name: "IBM Plex Mono",
+      cssVariable: "--font-plex",
+      provider: fontProviders.google(),
+    },
+    {
+      name: "Geist",
+      cssVariable: "--font-geist",
+      provider: fontProviders.google(),
+    },
+  ],
+  integrations: [sitemap()],
 });
---
+++ src/consts.ts
@@ -0,0 +1,40 @@
+export const SITE = {
+  URL: "https://your_site.com",
+  TITLE: "Mono Lume",
+  DESCRIPTION:
+    "A full-stack developer with a strong focus on building fast, accessible, and maintainable web applications.",
+  EMAIL: "hi@monolume.com",
+};
+
+export const HOME = {
+  TITLE: "Home",
+  DESCRIPTION:
+    "A personal blog about coding, building projects, and figuring things out",
+};
+
+export const BLOG = {
+  TITLE: "Blog",
+  DESCRIPTION:
+    "A collection of articles on programming, projects and other stuff",
+};
+
+export const PROJECTS = {
+  TITLE: "Projects",
+  DESCRIPTION:
+    "A collection of my projects with links to repositories and live demos.",
+};
+
+export const SOCIALS = [
+  {
+    NAME: "X",
+    HREF: "https://x.com/your_username",
+  },
+  {
+    NAME: "Linkedin",
+    HREF: "https://linkedin.com/",
+  },
+  {
+    NAME: "Github",
+    HREF: "https://github.com/your_username",
+  },
+];
